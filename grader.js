1ee7d11951a03c947935905cd72d8bfbccad7328
Colin McArthur <colin.mcarthur@mac.com>
2013-09-11 19:29:26 +0000

    enaled grader to view url's

--- grader.js
+++ grader.js
@@ -24,6 +24,10 @@ references:
 var fs = require('fs')
 var program = require('commander');
 var cheerio = require('cheerio');
+
+// for second part
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,8 +48,8 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(cheerio_object, checksfile) {
+    $ = cheerio_object;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for (var ii in checks) {
@@ -55,20 +59,62 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtmlFile = function(htmlfile, checksfile, callback) {
+    fs.readFile(htmlfile, function(err, data) {
+	if (err) {
+	    console.error('Error loading file: ' + htmlfile);
+	} else {
+	    var cheerio_object = cheerio.load(data);
+	    callback(checkHtml(cheerio_object, checksfile));
+        }
+    });
+};
+
+var checkHtmlUrl = function(url, checksfile, callback) {
+    rest.get(url)
+      .on('success', function(result, response) {
+	  var cheerio_object = cheerio.load(result);
+	  callback(checkHtml(cheerio_object, checksfile));
+	  })
+      .on('fail', function(data, response) {
+	  console.error('failure loading URL: ' + url);
+       });
+};
+
+
 var clone = function(fn) {
     // workaround for commandr.js issue
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var outputJson = function(json) {
+    var outJson = JSON.stringify(json, null, 4);
+    console.log(outJson);
+};
+
 if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html)file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <webpage_url>', 'URL for webpage to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
+
+/*    var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+}
+*/
+// accept either URL or file apth, with URL having precendence
+// note outputJson is a callback function
+    if (program.url !== undefined) {
+	checkHtmlUrl(program.url, program.checks, outputJson);
+    } else {
+	checkHtmlFile(program.file, program.checks, outputJson);
+    }
+} else {
+exports.checkHtmlFile = checkHtmlFile;
+exports.checkHtmlUrl = checkHtmlUrl;
 }
